c1d262b55eb177a416483f353493d4e0259c14ee
Fransiscus Xaverius <xaverius.fransiscus078@gmail.com>
2022-04-23 21:53:56 +0700

    Nambah JForm, edit main dikit
    nambah JForm

---
+++ DNF/src/main/java/com/mycompany/dnf/DNFMain.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.dnf;
+
+/**
+ *
+ * @author Dionisius, Felicia, Felix, Frans
+ */
+public class DNFMain {
+
+    public static void main(String[] args) {
+       
+    }
+    
+}
