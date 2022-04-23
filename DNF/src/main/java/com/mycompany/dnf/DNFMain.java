d02397e57b51baf845f9a830daebe1f590b2c468
fwijaya918 <fwijaya918@gmail.com>
2022-04-23 22:24:47 +0700

    Update DNFMain.java

--- DNF/src/main/java/com/mycompany/dnf/DNFMain.java
+++ DNF/src/main/java/com/mycompany/dnf/DNFMain.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package com.mycompany.dnf;
+
 import javax.swing.JFrame;
 import javax.swing.ImageIcon;
 
@@ -13,22 +14,22 @@ import javax.swing.ImageIcon;
 public class DNFMain {
 
     public static void main(String[] args) {
-       int p,l;
-       p = 2532;
-       l = 1170;
-       String title = "DNF";
-       String iconImageName = "";
-       JFrame loginFrame = new JFrame();
-       ImageIcon iconImg = new ImageIcon("smilink.png"); //tolong bikin logo trus diganti.
-       
-       loginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       loginFrame.setSize(p,l);
-       loginFrame.setTitle(title);
-       loginFrame.setIconImage(iconImg.getImage());
-       loginFrame.setVisible(true);
-       
-        System.out.println("Tes");
-       
+        int p, l;
+        p = 2532;
+        l = 1170;
+        String title = "DNF";
+        String iconImageName = "";
+        JFrame loginFrame = new JFrame();
+        ImageIcon iconImg = new ImageIcon("smilink.png"); //tolong bikin logo trus diganti.
+
+        loginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        loginFrame.setSize(p, l);
+        loginFrame.setTitle(title);
+        loginFrame.setIconImage(iconImg.getImage());
+        loginFrame.setVisible(true);
+
+        System.out.println("frans babi");
+
     }
-    
+
 }
