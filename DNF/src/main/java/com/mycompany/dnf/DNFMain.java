cc9d0398fd2c988838bbc1824f385a9b6699ec96
Fransiscus Xaverius <xaverius.fransiscus078@gmail.com>
2022-04-23 22:11:30 +0700

    Added JFRAME
    nambah JFRAME baru buat login

--- DNF/src/main/java/com/mycompany/dnf/DNFMain.java
+++ DNF/src/main/java/com/mycompany/dnf/DNFMain.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package com.mycompany.dnf;
+import javax.swing.JFrame;
+import javax.swing.ImageIcon;
 
 /**
  *
@@ -11,6 +13,19 @@ package com.mycompany.dnf;
 public class DNFMain {
 
     public static void main(String[] args) {
+       int p,l;
+       p = 2532;
+       l = 1170;
+       String title = "DNF";
+       String iconImageName = "";
+       JFrame loginFrame = new JFrame();
+       ImageIcon iconImg = new ImageIcon("smilink.png"); //tolong bikin logo trus diganti.
+       
+       loginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       loginFrame.setSize(p,l);
+       loginFrame.setTitle(title);
+       loginFrame.setIconImage(iconImg.getImage());
+       loginFrame.setVisible(true);
        
     }
     
