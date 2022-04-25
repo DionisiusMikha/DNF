e20914db07c8f11c2003294574e73acb54c6cf10
Fransiscus Xaverius <xaverius.fransiscus078@gmail.com>
2022-04-26 01:07:08 +0700

    Added private variable email to store user email

--- DNF/src/main/java/com/mycompany/dnf/User.java
+++ DNF/src/main/java/com/mycompany/dnf/User.java
@@ -9,13 +9,23 @@ package com.mycompany.dnf;
  * @author Frans
  */
 public class User {
-    String username;
-    String password;
+    private String username;
+    private String password;
+    private String email;
 
-    public User(String username, String password) {
+    public User(String username, String password, String email) {
         this.username = username;
         this.password = password;
+        this.email = email;
     }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }   
 
     public String getUsername() {
         return username;
