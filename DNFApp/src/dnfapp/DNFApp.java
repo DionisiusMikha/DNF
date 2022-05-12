fadc5764ec32fc073863778bcfb282f71527780d
Fransiscus Xaverius <58040241+Fransiscus-ID@users.noreply.github.com>
2022-05-12 22:06:45 +0700

    added new project file

---
+++ DNFApp/src/dnfapp/DNFApp.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package dnfapp;
+
+/**
+ *
+ * @author Frans
+ */
+public class DNFApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        LoginMenu LM = new LoginMenu();
+        LM.setVisible(true);
+        LM.pack();
+        LM.setLocationRelativeTo(null);
+        LM.setDefaultCloseOperation(LoginMenu.EXIT_ON_CLOSE);
+    }
+    
+}
