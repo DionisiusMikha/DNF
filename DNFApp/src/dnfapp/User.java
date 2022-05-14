5d998d6ba07ac9c27509d02f0c1c701246b2c078
Fransiscus Xaverius <xaverius.fransiscus078@gmail.com>
2022-05-15 03:47:38 +0700

    Added new java Classes, and added new JFrame for Account Creation
    Added:
    JAVA CLASS
    User
    Package
    
    Added JFrame
    CreateAccountPage.java

---
+++ DNFApp/src/dnfapp/User.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dnfapp;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ *
+ * @author Frans
+ */
+public class User {
+    String username;
+    String password;
+}
