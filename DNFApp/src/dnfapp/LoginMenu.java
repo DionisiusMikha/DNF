a71654fee9ffe41bffd4db496522ecf18dc95127
Fransiscus Xaverius <xaverius.fransiscus078@gmail.com>
2022-06-01 14:15:26 +0700

    Added ShowPasswordButton on LoginMenu Frame

--- DNFApp/src/dnfapp/LoginMenu.java
+++ DNFApp/src/dnfapp/LoginMenu.java
@@ -23,7 +23,7 @@ public class LoginMenu extends javax.swing.JFrame {
     private HashMap<String, String> usedEmail = new HashMap<String, String>();
     private HashMap<String, Package> DeliveryList = new HashMap<String, Package>();
     private HashMap<String, Kurir> ListKurir = new HashMap<String, Kurir>();
-    
+    private boolean ShowPass;
 
     /**
      * Creates new form LoginMenu
@@ -31,6 +31,7 @@ public class LoginMenu extends javax.swing.JFrame {
     public LoginMenu() {
         initComponents();
         LoginBG.requestFocus();
+        this.ShowPass=false;
     }
 
     //initial constructor, used to generate admin account.
@@ -41,6 +42,7 @@ public class LoginMenu extends javax.swing.JFrame {
             User admin = new User("admin", "admin", "CustomerService@DNF.co.id");
             userlist.put("admin", admin);
             System.out.println(userlist);
+            this.ShowPass=false;
         }
     }
 
@@ -50,6 +52,7 @@ public class LoginMenu extends javax.swing.JFrame {
         LoginBG.requestFocus();
         this.userlist = userlist;
         this.usedEmail = usedEmail;
+        this.ShowPass=false;
     }
 
     //situational, for compatibility issues.
@@ -59,6 +62,7 @@ public class LoginMenu extends javax.swing.JFrame {
         this.userlist = userlist;
         this.usedEmail = usedEmail;
         this.DeliveryList = DeliveryList;
+        this.ShowPass=false;
     }
 
     //main cons
@@ -69,6 +73,7 @@ public class LoginMenu extends javax.swing.JFrame {
         this.usedEmail = usedEmail;
         this.DeliveryList = DeliveryList;
         this.ListKurir = ListKurir;
+        this.ShowPass=false;
     }
 
     /**
@@ -84,7 +89,7 @@ public class LoginMenu extends javax.swing.JFrame {
         SignInButton = new javax.swing.JButton();
         usernamefield = new javax.swing.JTextField();
         PasswordField = new javax.swing.JPasswordField();
-        ToggleShowPassword = new javax.swing.JToggleButton();
+        ShowPassButton = new javax.swing.JButton();
         LoginBG = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -158,26 +163,15 @@ public class LoginMenu extends javax.swing.JFrame {
         });
         getContentPane().add(PasswordField, new org.netbeans.lib.awtextra.AbsoluteConstraints(720, 335, 320, 20));
 
-        ToggleShowPassword.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/eye.png"))); // NOI18N
-        ToggleShowPassword.setSelected(true);
-        ToggleShowPassword.setBorderPainted(false);
-        ToggleShowPassword.setContentAreaFilled(false);
-        ToggleShowPassword.addChangeListener(new javax.swing.event.ChangeListener() {
-            public void stateChanged(javax.swing.event.ChangeEvent evt) {
-                ToggleShowPasswordStateChanged(evt);
-            }
-        });
-        ToggleShowPassword.addMouseListener(new java.awt.event.MouseAdapter() {
+        ShowPassButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/eye.png"))); // NOI18N
+        ShowPassButton.setBorderPainted(false);
+        ShowPassButton.setContentAreaFilled(false);
+        ShowPassButton.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                ToggleShowPasswordMouseClicked(evt);
+                ShowPassButtonMouseClicked(evt);
             }
         });
-        ToggleShowPassword.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                ToggleShowPasswordActionPerformed(evt);
-            }
-        });
-        getContentPane().add(ToggleShowPassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(1040, 330, 30, -1));
+        getContentPane().add(ShowPassButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(1050, 330, 20, 18));
 
         LoginBG.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/Login_Menu.png"))); // NOI18N
         getContentPane().add(LoginBG, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, -190, -1, 1100));
@@ -287,24 +281,16 @@ public class LoginMenu extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_PasswordFieldFocusLost
 
-    private void ToggleShowPasswordActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ToggleShowPasswordActionPerformed
-        
-    }//GEN-LAST:event_ToggleShowPasswordActionPerformed
-
-    private void ToggleShowPasswordMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_ToggleShowPasswordMouseClicked
-        
-    }//GEN-LAST:event_ToggleShowPasswordMouseClicked
-
-    private void ToggleShowPasswordStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_ToggleShowPasswordStateChanged
-       if(this.ToggleShowPassword.isSelected()){
-            this.ToggleShowPassword.setSelected(false);
-            System.out.println("Was Selected, now unselected");
+    private void ShowPassButtonMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_ShowPassButtonMouseClicked
+        if(this.ShowPass==true){
+            this.ShowPass=false;
+            this.ShowPassButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/eye_crossed.png")));
         }
         else{
-            this.ToggleShowPassword.setSelected(true);
-            System.out.println("Was Unselected, now selected");
+            this.ShowPass=true;
+            ShowPassButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/eye.png")));
         }
-    }//GEN-LAST:event_ToggleShowPasswordStateChanged
+    }//GEN-LAST:event_ShowPassButtonMouseClicked
     
     
     
@@ -348,8 +334,8 @@ public class LoginMenu extends javax.swing.JFrame {
     private javax.swing.JButton CreateAccButton;
     private javax.swing.JLabel LoginBG;
     private javax.swing.JPasswordField PasswordField;
+    private javax.swing.JButton ShowPassButton;
     private javax.swing.JButton SignInButton;
-    private javax.swing.JToggleButton ToggleShowPassword;
     private javax.swing.JTextField usernamefield;
     // End of variables declaration//GEN-END:variables
 }
